cc0380359baeeb2ae22fd6e5b4cfccbf370e1e89
shuailongcheng <shuailongcheng@yoloyolo.tv>
2017-02-14 15:28:49 +0800

    no message

---
+++ weaponlib/src/main/java/com/github/h0ngyue/weaponlib/Test.java
@@ -0,0 +1,13 @@
+package com.github.h0ngyue.weaponlib;
+
+import android.util.Log;
+
+/**
+ * Created by shuailongcheng on 14/02/2017.
+ */
+
+public class Test {
+    public static void testFunc() {
+        Log.d("test", "testFunc");
+    }
+}
