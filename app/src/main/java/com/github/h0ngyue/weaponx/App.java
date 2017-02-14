a348136adf6bd6f4c9fcf6d9161845c0e4d389ca
shuailongcheng <shuailongcheng@yoloyolo.tv>
2017-02-14 15:44:50 +0800

    no message

---
+++ app/src/main/java/com/github/h0ngyue/weaponx/App.java
@@ -0,0 +1,14 @@
+package com.github.h0ngyue.weaponx;
+
+import android.util.Log;
+
+/**
+ * Created by shuailongcheng on 14/02/2017.
+ */
+
+public class App {
+
+    public static void testApp() {
+        Log.d("app", "app");
+    }
+}
